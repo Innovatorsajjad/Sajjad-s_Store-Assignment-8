afed6db5bc7a874790744486261e6ebd8018ba64
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-03-26 17:49:17 +0600

    cart icon added

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,11 +1,15 @@
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import "./Product.css"
-const Product = (props) => {
-    const{name,img,seller,price,ratings}=props.product
+const Product = ({handleAddToCart,product}) => {
+  // const{handleAddToCart,product}=props
+    const{name,img,seller,price,ratings}= product;
+
     return (
         <div class="col">
     <div class="card single_card">
-      <img src={img}class="card-img-top img-fluid" alt="..."/>
+      <img src={img}class="card-img-top" alt="..."/>
       <div class="card- text-center">
         <h2 class="card-title">{name}</h2>
         <h5 class="card-text"> Saller: {seller} </h5>
@@ -13,7 +17,10 @@ const Product = (props) => {
       </div>
       <small class="text-muted">Ratings : {ratings} stars</small>
       <div class="">
-        <button className='buy_now_Button w-100 text-light' > <a href="#">Buy Now</a> </button>
+        <button onClick={ ()=> handleAddToCart (product)} className='buy_now_Button w-100 text-light' >
+           <a href="#">Buy Now  
+           <FontAwesomeIcon icon={faShoppingCart} ></FontAwesomeIcon></a>
+         </button>
       </div>
     </div>
   </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -5,24 +5,31 @@ import "./Shop.css"
 const Shop = () => {
 
     const[products,setProducts]=useState([]);
+    const [cart,setCart]=useState([]);
     useEffect(()=>{
         fetch ("products.json")
         .then(Response=>Response.json())
         .then(data=>setProducts(data))
     },[])
+    const handleAddToCart =(product)=>{
+        
+        const newCart=[...cart,product]
+        setCart(newCart)
+      }
     return (
         <div className=' container' >
        <div className="row">
-       <div className=" col-md-10 row row-cols-1 row-cols-md-3 g-4"> 
+       <div className=" product_container col-md-10 row row-cols-1 row-cols-md-3 g-4"> 
          {
              products.map(product=><Product
              key={product.id}
              product ={product}
+             handleAddToCart={handleAddToCart}
              ></Product>)
          } 
         </div>
             <div className=" col-md-2">
-                this is cart container
+                <h3>Cart Name = {cart.length}</h3>
              </div>
        </div>
         </div>
