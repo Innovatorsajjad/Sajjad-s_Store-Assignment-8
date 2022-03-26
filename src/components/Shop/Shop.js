2c55678751e50dc29ce2a003232f9061a2c0c1eb
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-03-26 10:10:52 +0600

    products loop added

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Product from '../Product/Product';
+import "./Shop.css"
+
+const Shop = () => {
+
+    const[products,setProducts]=useState([]);
+    useEffect(()=>{
+        fetch ("products.json")
+        .then(Response=>Response.json())
+        .then(data=>setProducts(data))
+    },[])
+    return (
+        <div className='shop_container' >
+        <div className="product_container"> 
+         {
+             products.map(productsi=><Product></Product>)
+         } 
+        </div>
+            <div className="cart_container">
+                this is cart container
+                </div>
+        </div>
+    );
+};
+
+export default Shop;
