9c0b3f30e48f5f773535cc431192f5771205285d
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-03-26 08:17:28 +0600

    navBar added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import"./Header.css"
+
+const Header = () => {
+    return (
+        <nav className='nav'  >
+            <h1 className='tittleName'><span className='colorName'>Sajjad</span>'s Store</h1>
+        </nav>
+    );
+};
+
+export default Header;
