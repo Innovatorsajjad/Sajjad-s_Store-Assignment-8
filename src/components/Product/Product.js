8ae37e47606814607e53a8cd5a5dfd6af84a2422
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-03-26 13:02:58 +0600

    card edited successfully

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -4,15 +4,16 @@ const Product = (props) => {
     const{name,img,seller,price,ratings}=props.product
     return (
         <div class="col">
-    <div class="card h-100 single_card">
-      <img src={img}class="card-img-top" alt="..."/>
+    <div class="card single_card">
+      <img src={img}class="card-img-top img-fluid" alt="..."/>
       <div class="card- text-center">
         <h2 class="card-title">{name}</h2>
-        <h5 class="card-text"> Saller {seller} </h5>
+        <h5 class="card-text"> Saller: {seller} </h5>
+        <h6>Price : {price}</h6>
       </div>
       <small class="text-muted">Ratings : {ratings} stars</small>
-      <div class="card-footer">
-        <button className='w-100' >Buy Now</button>
+      <div class="">
+        <button className='buy_now_Button w-100 text-light' >Buy Now</button>
       </div>
     </div>
   </div>
